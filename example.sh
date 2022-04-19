480aac205ad9891e9b66f0d1c08153c078646054
Mihran <44965305+m-minasyan@users.noreply.github.com>
2022-04-19 13:08:59 +0400

    Update example.sh

--- example.sh
+++ example.sh
@@ -1,7 +1,7 @@
 sudo docker ps -aq | xargs sudo docker stop | xargs sudo docker rm -f
 # Delete all images exept starting with node, nginx
 sudo docker images -a | grep -v '^node' | grep -v '^nginx' | awk 'NR>1 {print $3}' | xargs sudo docker image rm -f
-sudo docker volume ls -qf | xargs sudo docker volume rm -f
+sudo docker volume ls -q | xargs sudo docker volume rm -f
 sudo service docker stop
 sudo rm -rf /var/lib/docker/network
 sudo service docker start
